628f784bccb603b770a0d2f3b43d93cf03abab16
Komal Madiwal <komalmadiwal10@gmail.com>
2023-11-21 15:11:21 +0530

    file modified

--- src/DimondPricePrediction/components/model_trainer.py
+++ src/DimondPricePrediction/components/model_trainer.py
@@ -14,7 +14,7 @@ from sklearn.linear_model import LinearRegression, Ridge,Lasso,ElasticNet
 
 @dataclass 
 class ModelTrainerConfig:
-    trained_model_file_path = os.path.join('artifacts','model.pkl')
+    trained_model_file_path = os.path.join('artifacts','model.pkl') ##saving model.pkl file in artifacts folder
     
     
 class ModelTrainer:
@@ -25,10 +25,10 @@ class ModelTrainer:
         try:
             logging.info('Splitting Dependent and Independent variables from train and test data')
             X_train, y_train, X_test, y_test = (
-                train_array[:,:-1],
-                train_array[:,-1],
-                test_array[:,:-1],
-                test_array[:,-1]
+                train_array[:,:-1],### x train = all rows and columns except last 
+                train_array[:,-1],## y train = all rows and last col
+                test_array[:,:-1],## x test
+                test_array[:,-1]## y test
             )
 
             models={
@@ -37,18 +37,20 @@ class ModelTrainer:
             'Ridge':Ridge(),
             'Elasticnet':ElasticNet()
         }
-            
-            model_report:dict=evaluate_model(X_train,y_train,X_test,y_test,models)
+            ##evaluate_model function is defined in the utils module.
+            model_report:dict=evaluate_model(X_train,y_train,X_test,y_test,models) ## This is a variable that will store the output of the 
+            #evaluate_model function.dict: It specifies the expected type of the variable. In this context, it means that model_report is expected to be a dictionary.
+            ##models: A dictionary containing regression models.
             print(model_report)
             print('\n====================================================================================\n')
             logging.info(f'Model Report : {model_report}')
 
             # To get best model score from dictionary 
-            best_model_score = max(sorted(model_report.values()))
+            best_model_score = max(sorted(model_report.values()))##This extracts the values (R-squared scores) from the model_report dictionary
 
             best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
-            ]
+            ]##list(model_report.values()).index(best_model_score) calculates the index of the highest R-squared score in the list of R-squared scores.
             
             best_model = models[best_model_name]
 
@@ -59,7 +61,7 @@ class ModelTrainer:
             save_object(
                  file_path=self.model_trainer_config.trained_model_file_path,
                  obj=best_model
-            )
+            )## save at artifacts folder
           
 
         except Exception as e:
